dd0c5cf55132ae7da416abbf1adfe1a28d4e91db
Langlois <david.langlois@loria.fr>
2018-08-27 14:59:04 +0200

    fonction changer_nom, TODO en commentaire

---
+++ POO/mediatheque.py
@@ -0,0 +1,33 @@
+
+
+##liste d'usagers
+##id, nom, prénom, date naissance, liste des emprunts
+##
+##liste des emprunts = (id,debut,fin_attendue,fin_reelle = None)*
+##
+##
+##livres
+##(id, titre, auteur, liste_mots_cles)
+##
+##
+##Exos
+##Y- charger à partir de fichier (csv) x3
+##D- changer le nom d'un usager à partir de son id
+##Y- ajoyter un mot clé à un livre
+##D- les majeurs (1 liste)
+##Y- les en retard (1 liste)
+##D- les livres sur un mot clé en paramètre (1 liste)
+##Y- titres des livres actuellement empruntés (2 listes)
+##D- le livre le plus récemment rendus de X (2 listes)
+##Y- liste des personnes ayant emprunté le livre d'iditifiant X (2 listes)
+##D- liste des personnes ayant emprunté le livre de titre X (3 listes)
+##Y- la liste des mots clés (sans doublon) emprunté par X --> enjeu sociétaux
+##DY- (bonus) le nombre max de livres empruntés en même temps par quelqu'un
+##Y- fiche identité usager (3 listes)
+
+
+def changer_nom(liste_usagers,id_usager,nv_nom):
+    for i in range(liste_usagers):
+        if liste_usagers[i][0] == id_usager:
+            liste_usagers[i][1] =  nv_nom
+
